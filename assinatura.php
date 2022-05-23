8bb4636ade4235c219082574c5464d00c0cf86c1
fernandofm3 <fernandofm3@gmail.com>
2022-05-23 14:38:26 -0300

    Foi corrigido a apresentação das informações na tela (não estava aparecendo o nome, setor, tel), e foi melhorado a qualidade da imagem.

--- assinatura.php
+++ assinatura.php
@@ -7,8 +7,9 @@ $telefone = $_POST['telefone'];
 
 //Criando a imagem
 header("Content-type: image/png");
+
 //Usando uma imagem existente
-$im = imagecreatefromjpeg("img/ass.jpg");
+$im = imagecreatefromjpeg("./img/ass.jpg");
 
 //Cor do fundo da imagem
 $fundo = imagecolorallocate($im, 255, 255, 255);
@@ -19,10 +20,11 @@ $preto = imagecolorallocate($im, 17, 54, 109);
 //Informações da assinatura
 $texto = "$nome 
 $setor \n
-+55 (11) $telefone";
++55 (11) $telefone \n
+www.meucolirio.com.br";
 
-$fonte = "Arial.ttf";
+$fonte = "./Arial.ttf";
 
-imagettftext($im, 8, 0, 3, 14, $preto, $fonte, $texto);
-imagepng($im);
+imagettftext($im, 14, 0, 6, 20, $preto, $fonte, $texto);
+imagejpeg($im);
 imagedestroy($im);
